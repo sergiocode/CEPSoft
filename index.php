c3971087303f74f12e181923ef2801b54565e32c
sergiocode <sergiomr.info@gmail.com>
2021-11-01 20:29:43 +0100

    Comienzo de la Landing
    NavBar sin terminar, falta responsive. Background-image de prueba.

--- index.php
+++ index.php
@@ -1,12 +1,33 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
+    <title>Centre d'Estudis Politècnics</title>
+    <link rel="stylesheet" href="./style/styles.css">
 </head>
 <body>
-    
+    <nav>
+        <div class="navLogo">
+            <img src="./media/img/logoCEP.png" alt="">
+            <h3>Centre d'Estudis<br/>Politècnics</h3>
+        </div>
+        <ul class="navLinks">
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Juegos</a></li>
+            <li><a href="#">Contacto</a></li>
+            <li><a href="#">ADMIN</a></li>
+            <li><a class="loginBtn" href="#" id="loginBtn">Inciar sesión</a></li>
+        </ul>
+        <div class="burger">
+            <div class="line1"></div>
+            <div class="line2"></div>
+            <div class="line3"></div>
+        </div>
+    </nav>
 </body>
 </html>
