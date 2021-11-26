e3f06050980d45ba99cf32c1fdb022b1a57659d9
sergiocode <92025078+sergiocode@users.noreply.github.com>
2021-11-26 19:46:27 +0100

    26nov
    Añadiendo archivos actualizados de la Landing a la branch Sergio.
    Página home actualizada con responsive (falta descomentar parte del código). Página de juegos en fase avanzada más responsive.

---
+++ js/navbarResponsive.js
@@ -0,0 +1,36 @@
+const navBar = document.querySelector('nav');
+const navLinkDesktop = document.querySelectorAll('.navLink-desktop');
+const cepTitle = document.querySelector('.cepTitle');
+const burger = document.querySelector('.burger');
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.navLinks');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        // navBar.style.backgroundColor = "white";
+        // cepTitle.style.color = "black";
+        // navLink.style.color = "black";
+        // burger.style.color = "black";
+    });
+}
+
+navSlide();
+
+window.addEventListener('scroll', function(){
+    if(window.pageYOffset > 500){
+        console.log('Hola');
+        navBar.style.backgroundColor = "white";
+        navLinkDesktop.forEach(element => {
+            element.style.color = "black";
+        });
+        cepTitle.style.color = "black";
+    } else {
+        navBar.style.backgroundColor = "#ffffff1f";
+        navLinkDesktop.forEach(element => {
+            element.style.color = "WHITE";
+        });
+        cepTitle.style.color = "white";
+    }
+});
